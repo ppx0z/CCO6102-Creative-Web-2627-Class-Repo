69b60dfa0df5aed9f978bd4ac7b957f0afe73a6b
LJ <lj.marquez24@bathspa.ac.uk>
2026-09-22 21:35:08 +0100

    setting up

---
+++ Week 1/00-svelte-basic/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+let control = $state(0);
+ let count = $state(0);
+
+ function increment() {
+   count+= 1;
+   console.log('new count value', count);
+ }
+</script>
+
+<div>
+<p> The count is {count}</p>
+<button on:click={increment}>Add</button>
+</div>
